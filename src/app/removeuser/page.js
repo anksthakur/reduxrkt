bfebfe8e5011492ba177ba3be0ff010989d5a6fc
anksthakur <anksthakur7@gmail.com>
2024-03-20 12:22:02 +0530

    update

--- src/app/removeuser/page.js
+++ src/app/removeuser/page.js
@@ -1,17 +1,19 @@
 "use client"
-import { useDispatch, useSelector } from "react-redux"
+import React from "react"; // Import React
+import { useDispatch, useSelector } from "react-redux";
 import { removeUser } from "../redux/slice";
 
 export default function Page(){
-    const userData = useSelector((data)=>data.usersData.users)
-    const dispatch = useDispatch();
+    const userData = useSelector((data)=>data.usersData.users) || []; // Adding null check
+    const dispatch = useDispatch(); // Get dispatch function from useDispatch hook
+
     console.log(userData)
     return(
         <div className="display-user">
             <h1> Remove user page </h1>
             {
-                userData.map((item)=>(
-                    <div className="user-item">
+                userData.map((item, index)=>(
+                    <div className="user-item" key={index}> {/* Adding key prop */}
                         <span>{item.name}</span>
                         <button onClick={()=>dispatch(removeUser(item.id))}>Remove</button>
                     </div>
@@ -20,3 +22,5 @@ export default function Page(){
         </div>
     )
 }
+
+
